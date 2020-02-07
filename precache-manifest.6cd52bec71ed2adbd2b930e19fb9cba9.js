af39351654d4680a7b57dd5d589f1a34bf9a686a
Martirossyan Ararat <myfailemtions@gmail.com>
2020-02-07 11:18:35 +0100

    Updates

---
+++ precache-manifest.6cd52bec71ed2adbd2b930e19fb9cba9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91727479474c4d7563296df929014196",
+    "url": "/index.html"
+  },
+  {
+    "revision": "948e1ec301860ee1d892",
+    "url": "/static/css/2.73d444c6.chunk.css"
+  },
+  {
+    "revision": "58456f911804afd786e1",
+    "url": "/static/css/main.93cb30c3.chunk.css"
+  },
+  {
+    "revision": "948e1ec301860ee1d892",
+    "url": "/static/js/2.25f50fc3.chunk.js"
+  },
+  {
+    "revision": "60bc45ccae0a30dcd1a1221fbca31d4c",
+    "url": "/static/js/2.25f50fc3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "58456f911804afd786e1",
+    "url": "/static/js/main.39feb55a.chunk.js"
+  },
+  {
+    "revision": "3fb5de16e5e718b443ee",
+    "url": "/static/js/runtime-main.65e5ea41.js"
+  }
+]);
