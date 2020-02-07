cd9743cb3b2c5ba61cbeb18d0d6bc7c1c4f0612f
Martirossyan Ararat <myfailemtions@gmail.com>
2020-02-07 11:25:40 +0100

    Updates

---
+++ precache-manifest.6ce64afb176803146aba9b42cd8e325f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d31f09befcc1d0fb2875d39e2795cac",
+    "url": "/myfailemtions/movieology-pwa/index.html"
+  },
+  {
+    "revision": "dc5c580438554fd0bdd5",
+    "url": "/myfailemtions/movieology-pwa/static/css/2.73d444c6.chunk.css"
+  },
+  {
+    "revision": "4d403d36a4752d67c91a",
+    "url": "/myfailemtions/movieology-pwa/static/css/main.812c523a.chunk.css"
+  },
+  {
+    "revision": "dc5c580438554fd0bdd5",
+    "url": "/myfailemtions/movieology-pwa/static/js/2.00e207a5.chunk.js"
+  },
+  {
+    "revision": "60bc45ccae0a30dcd1a1221fbca31d4c",
+    "url": "/myfailemtions/movieology-pwa/static/js/2.00e207a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d403d36a4752d67c91a",
+    "url": "/myfailemtions/movieology-pwa/static/js/main.202b3c82.chunk.js"
+  },
+  {
+    "revision": "ce5b99da8bbaaa57018d",
+    "url": "/myfailemtions/movieology-pwa/static/js/runtime-main.c1e574e1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6cd52bec71ed2adbd2b930e19fb9cba9.js"
+  "/myfailemtions/movieology-pwa/precache-manifest.6ce64afb176803146aba9b42cd8e325f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myfailemtions/movieology-pwa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
